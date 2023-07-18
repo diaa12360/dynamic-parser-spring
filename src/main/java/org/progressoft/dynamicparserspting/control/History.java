100e24a94d048d28985bd1f39e1b532c32623d23
diaa12360 <deyacc12@gmail.com>
2023-07-18 23:24:43 +0300

    add JPA And It's work now

--- src/main/java/org/progressoft/dynamicparserspting/control/History.java
+++ src/main/java/org/progressoft/dynamicparserspting/control/History.java
@@ -3,17 +3,13 @@ package org.progressoft.dynamicparserspting.control;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.progressoft.dynamicparserspting.connection.DatabaseConnection;
 import org.progressoft.dynamicparserspting.connection.HistoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,34 +20,20 @@ public class History {
     HistoryRepo history;
     @PostMapping
     protected void post(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            List<org.progressoft.dynamicparserspting.connection.History> data = history.findAll();
-            Connection connection = new DatabaseConnection("mydb").setConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("select * from history_table");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            ArrayList<String> DbColumnNames = new ArrayList<>();
-            for (int i = 1; i <= 3; i++) {
-                DbColumnNames.add(metaData.getColumnName(i));
-            }
-            req.setAttribute("DbColumnNames", DbColumnNames);
-            ArrayList<ArrayList<String>> tableData = new ArrayList<>();
-            int x = 0;
-            while (resultSet.next()){
-                tableData.add(new ArrayList<>());
-                tableData.get(x).add(data.get(x).getFileName());
-                tableData.get(x).add(data.get(x).getSummation());
-                tableData.get(x).add(data.get(x).getAverage());
-                x++;
-            }
-            System.out.println(tableData);
-            req.setAttribute("tableData", tableData);
-            req.getRequestDispatcher("/WEB-INF/views/historyPage.jsp").forward(req, resp);
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+        List<org.progressoft.dynamicparserspting.connection.History> data = history.findAll();
+        ArrayList<ArrayList<String>> tableData = new ArrayList<>();
+        for (int i = 0; i < data.size(); i++){
+            tableData.add(new ArrayList<>());
+            tableData.get(i).add(data.get(i).getFileName());
+            tableData.get(i).add(data.get(i).getSummation());
+            tableData.get(i).add(data.get(i).getAverage());
         }
+        req.setAttribute("tableData", tableData);
+        ArrayList<String> DbColumnNames = new ArrayList<>();
+        DbColumnNames.add("File Name");
+        DbColumnNames.add("Summation");
+        DbColumnNames.add("Average");
+        req.setAttribute("DbColumnNames", DbColumnNames);
+        req.getRequestDispatcher("/WEB-INF/views/historyPage.jsp").forward(req, resp);
     }
 }
--- src/main/java/org/progressoft/dynamicparserspting/control/WebConfig.java
+++ src/main/java/org/progressoft/dynamicparserspting/control/WebConfig.java
@@ -2,16 +2,10 @@ package org.progressoft.dynamicparserspting.control;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
